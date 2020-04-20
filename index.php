9fa52c600703e11b72664991b805e1a07944ba66
nanospez <56999301+nanospez@users.noreply.github.com>
2020-04-20 11:16:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,585 @@
+<?php 
+
+$title="home";
+
+$pagename="home";
+
+include("st_files/nav_page.php") ?>
+
+		<!-- Carousel -->
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			</ol>
+
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+
+				<!-- Slide #1 -->
+				<div class="carousel-item modern-lyo-slide bg1-modern active">
+					<div class="container">
+						<!-- Title -->
+						<div class="title-bg1-modern">
+							<h1>The
+								<b>Best</b> News Website.</h1>
+							<h2>Starting at any Time.</h2>
+						</div>
+						<!-- Features -->
+						<div class="features-bg1-modern">
+							<!-- Item -->
+							<div class="feature-modlyo-item">
+								<i class="fa fa-gift"></i>
+								<span>FREE Domain Registration</span>
+							</div>
+							<!-- Item -->
+							<div class="feature-modlyo-item">
+								<i class="fa fa-object-group"></i>
+								<span>The easiest Drag &amp; Drop sitebuilder</span>
+							</div>
+							<!-- Item -->
+							<div class="feature-modlyo-item">
+								<i class="fa fa-wordpress"></i>
+								<span>1 click Wordpress and 300+ open source apps installer</span>
+							</div>
+							<!-- Item -->
+							<div class="feature-modlyo-item">
+								<i class="fa fa-support"></i>
+								<span>24/7/365 Genius Support® assistance</span>
+							</div>
+						</div>
+						<!-- More Details -->
+						<div class="details-bg1-modern">
+							<a class="getstarted-modernlyo" href="#">Get Started</a>
+							<a class="seeplans-modernlyo" href="#">or See Plans</a>
+						</div>
+					</div>
+				</div>
+
+				<!-- Slide #2 -->
+				<div class="carousel-item modern-lyo-slide bg2-modern">
+					<div class="container">
+						<!-- Title -->
+						<div class="title-bg1-modern">
+							<h1>The
+								<b>Best</b> Web Support</h1>
+							<h2>We keep your servers rock solid &amp; customers delighted</h2>
+						</div>
+						<!-- Features -->
+						<div class="features-bg1-modern">
+							<!-- Item -->
+							<div class="support-modlyo-item">
+								<i class="fa fa-money"></i>
+								<span>Billing</span>
+								<a href="#">Billing FAQS</a>
+							</div>
+							<!-- Item -->
+							<div class="support-modlyo-item">
+								<i class="fa fa-database"></i>
+								<span>MySQL</span>
+								<a href="#">Billing FAQS</a>
+							</div>
+							<!-- Item -->
+							<div class="support-modlyo-item">
+								<i class="fa fa-globe"></i>
+								<span>DNS</span>
+								<a href="#">DNS FAQS</a>
+							</div>
+							<!-- Item -->
+							<div class="support-modlyo-item">
+								<i class="fa fa-lock"></i>
+								<span>SSL</span>
+								<a href="#">SSL FAQS</a>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+						<!-- More Details -->
+						<div class="details-bg1-modern">
+							<a class="getstarted-modernlyo" href="#">Get Started</a>
+							<a class="seeplans-modernlyo" href="#">or See Plans</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="clearfix"></div>
+
+		<!-- Hosting Plans -->
+		<section class="plans-prices-st2">
+			<!-- Title -->
+			<div class="sec-title">
+				<h2>Featured Plans</h2>
+				<p>Innovative features make hosting simple.</p>
+			</div>
+			<div class="container">
+				<div class="row">
+					<!-- #STARTER PLAN -->
+					<div class="col-lg-3 col-sm-6">
+						<div class="plan-st2">
+							<!-- Title -->
+							<h3>Standard</h3>
+							<!-- Price -->
+							<span>3.9$ /mo</span>
+							<!-- Icon -->
+							<i class="fa fa-hdd-o mlyo-icon-st2"></i>
+							<!-- Features -->
+							<ul>
+								<li>
+									<a>10GB Bandwidth</a>
+								</li>
+								<li>
+									<a>1TB Storage Space</a>
+								</li>
+								<li>
+									<a>10 Free Sub-domains</a>
+								</li>
+								<li>
+									<a>Cpanel &amp; FTP</a>
+								</li>
+								<li>
+									<a>99.9% Uptime</a>
+								</li>
+								<li>
+									<a>Mysql Databases</a>
+								</li>
+							</ul>
+							<!-- Order -->
+							<a class="order-st2" href="#">Order</a>
+						</div>
+					</div>
+					<!-- #Enterprise PLAN -->
+					<div class="col-lg-3 col-sm-6">
+						<div class="plan-st2">
+							<!-- Title -->
+							<h3>Enterprise</h3>
+							<!-- Price -->
+							<span>9.9$ /mo</span>
+							<!-- Icon -->
+							<i class="fa fa-server mlyo-icon-st2"></i>
+							<!-- Features -->
+							<ul>
+								<li>
+									<a>10GB Bandwidth</a>
+								</li>
+								<li>
+									<a>1TB Storage Space</a>
+								</li>
+								<li>
+									<a>10 Free Sub-domains</a>
+								</li>
+								<li>
+									<a>Cpanel &amp; FTP</a>
+								</li>
+								<li>
+									<a>99.9% Uptime</a>
+								</li>
+								<li>
+									<a>Mysql Databases</a>
+								</li>
+							</ul>
+							<!-- Order -->
+							<a class="order-st2" href="#">Order</a>
+						</div>
+					</div>
+					<!-- #professional PLAN -->
+					<div class="col-lg-3 col-sm-6">
+						<div class="plan-st2 active-st2">
+							<!-- Title -->
+							<h3>Professional</h3>
+							<!-- Price -->
+							<span>13.9$ /mo</span>
+							<!-- Icon -->
+							<i class="fa fa-cloud mlyo-icon-st2"></i>
+							<!-- Features -->
+							<ul>
+								<li>
+									<a>10GB Bandwidth</a>
+								</li>
+								<li>
+									<a>1TB Storage Space</a>
+								</li>
+								<li>
+									<a>10 Free Sub-domains</a>
+								</li>
+								<li>
+									<a>Cpanel &amp; FTP</a>
+								</li>
+								<li>
+									<a>99.9% Uptime</a>
+								</li>
+								<li>
+									<a>Mysql Databases</a>
+								</li>
+							</ul>
+							<!-- Order -->
+							<a class="order-st2" href="#">Order</a>
+						</div>
+					</div>
+					<!-- #Company PLAN -->
+					<div class="col-lg-3 col-sm-6">
+						<div class="plan-st2">
+							<!-- Title -->
+							<h3>Company</h3>
+							<!-- Price -->
+							<span>13.9$ /mo</span>
+							<!-- Icon -->
+							<i class="fa fa-database mlyo-icon-st2"></i>
+							<!-- Features -->
+							<ul>
+								<li>
+									<a>10GB Bandwidth</a>
+								</li>
+								<li>
+									<a>1TB Storage Space</a>
+								</li>
+								<li>
+									<a>10 Free Sub-domains</a>
+								</li>
+								<li>
+									<a>Cpanel &amp; FTP</a>
+								</li>
+								<li>
+									<a>99.9% Uptime</a>
+								</li>
+								<li>
+									<a>Mysql Databases</a>
+								</li>
+							</ul>
+							<!-- Order -->
+							<a class="order-st2" href="#">Order</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Domain Register -->
+		<form class="dom-area hp">
+			<!-- Container -->
+			<div class="container">
+				<!-- Title -->
+				<div class="sec-title">
+					<h2>Domain Search</h2>
+					<p>Our domain manager makes it easy to track, update, transfer, purchase, and administer all of your domains in one place.</p>
+				</div>
+				<div class="row">
+					<!-- Domain Search -->
+					<div class="col-lg-4 col-sm-9">
+						<input class="sea-dom" type="search" placeholder="Enter your domain">
+					</div>
+					<!-- Extentions -->
+					<div class="col-lg-2 col-sm-3">
+						<select class="ext-d" name="extintions" id="ext">
+							<option value=".COM">.COM</option>
+							<option value=".NET">.NET</option>
+							<option value=".ORG">.ORG</option>
+							<option value=".INFO">.INFO</option>
+							<option value=".CLUB">.CLUB</option>
+						</select>
+					</div>
+					<!-- Btns -->
+					<div class="col-lg-2 col-xs-4">
+						<input class="reg-dom" type="submit" value="Register">
+					</div>
+					<div class="col-lg-2 col-xs-4">
+						<input class="tra-dom" type="submit" value="Transfer">
+					</div>
+					<div class="col-lg-2 col-xs-4">
+						<input class="mna-dom" type="submit" value="Manage">
+					</div>
+				</div>
+			</div>
+		</form>
+
+		<div class="container">
+			<!-- Features -->
+			<section class="features">
+				<!-- Title -->
+				<div class="sec-title">
+					<h2>Features</h2>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+				</div>
+				<!-- Nav tabs -->
+				<div class="row">
+					<ul class="nav-xdata nav nav-tabs custom" id="featuresTab" role="tablist">
+						<li class="nav-item col-lg-3 col-xs-12 col-sm-3">
+							<a class="nav-link active" href="#shared" aria-controls="shared" role="tab" data-toggle="tab">
+								<img src="img/icons/shared.svg" alt="shared hosting"> shared hosting</a>
+						</li>
+						<li class="nav-item col-lg-3 col-xs-12 col-sm-3">
+							<a class="nav-link" href="#vps" aria-controls="vps" role="tab" data-toggle="tab">
+								<img src="img/icons/vps.svg" alt="shared hosting"> vps hosting</a>
+						</li>
+						<li class="nav-item col-lg-3 col-xs-12 col-sm-3">
+							<a class="nav-link" href="#dedicated" aria-controls="dedicated" role="tab" data-toggle="tab">
+								<img src="img/icons/dedicated.svg" alt="shared hosting"> dedicated hosting</a>
+						</li>
+						<li class="nav-item col-lg-3 col-xs-12 col-sm-3">
+							<a class="nav-link" href="#twork" aria-controls="twork" role="tab" data-toggle="tab">
+								<img src="img/icons/twork.svg" alt="shared hosting"> team work</a>
+						</li>
+					</ul>
+					<!-- Tab panes -->
+					<div class="tab-content custom" id="featuresTabContent">
+						<!-- Shared Hosting -->
+						<div role="tabpanel" class="tab-pane fade show active" id="shared">
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-rocket feat"></span>Extreme Performance</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-server feat"></span>Instant Provisioning</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-sliders feat"></span>Enhanced Cpanel</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+						</div>
+						<!-- Vps Hosting -->
+						<div role="tabpanel" class="tab-pane fade" id="vps">
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-commenting feat"></span>24/7 Support</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-money feat"></span>Best Prices</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-cloud-download feat"></span>Backup &amp; Storage</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+						</div>
+						<!-- Dedicated Hosting -->
+						<div role="tabpanel" class="tab-pane fade" id="dedicated">
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-lock feat"></span>Security</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-dashboard feat"></span>99% UPTIME</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-chain-broken feat"></span>Unlimited Bandwith</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+						</div>
+						<!-- TeamWork -->
+						<div role="tabpanel" class="tab-pane fade" id="twork">
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-group feat"></span>Pro-Team</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-child feat"></span>Happy Customers</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+							<!-- Feature Block -->
+							<div class="block-fea">
+								<h3>
+									<span class="fa fa-cog feat"></span>Customizing</h3>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Faq & Testimonial -->
+			<section class="faq-testi">
+				<h2 class="valid">Validation Purposes</h2>
+				<!-- T-borders -->
+				<div class="l-border"></div>
+				<div class="r-border"></div>
+				<!-- Faq -->
+				<div class="l-faq-block-up">
+					<div class="panel-group custom" id="accordion">
+						<!-- Faq #1 -->
+						<div class="panel custom">
+							<div class="panel-heading custom" role="tab" id="headingOne">
+								<h4 class="panel-title custom">
+									<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+										How do I cancel and delete my account?
+										<span class="fa fa-plus-circle coll-a"></span>
+									</a>
+								</h4>
+							</div>
+							<div id="collapseOne" class="panel-collapse collapse show custom" aria-labelledby="headingOne" data-parent="#accordion">
+								<div class="panel-body custom">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+									book.
+								</div>
+							</div>
+						</div>
+						<!-- Faq #2 -->
+						<div class="panel custom">
+							<div class="panel-heading custom" role="tab" id="headingTwo">
+								<h4 class="panel-title custom">
+									<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+										Can I get my website listed in Google?
+										<span class="fa fa-plus-circle coll-a"></span>
+									</a>
+								</h4>
+							</div>
+							<div id="collapseTwo" class="panel-collapse collapse custom" aria-labelledby="headingTwo" data-parent="#accordion">
+								<div class="panel-body custom">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+									book.
+								</div>
+							</div>
+						</div>
+						<!-- Faq #3 -->
+						<div class="panel custom">
+							<div class="panel-heading custom" role="tab" id="headingThree">
+								<h4 class="panel-title custom">
+									<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+										Can I run a business?
+										<span class="fa fa-plus-circle coll-a"></span>
+									</a>
+								</h4>
+							</div>
+							<div id="collapseThree" class="panel-collapse collapse custom" aria-labelledby="headingThree" data-parent="#accordion">
+								<div class="panel-body custom">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+									book.
+								</div>
+							</div>
+						</div>
+						<!-- Faq #4 -->
+						<div class="panel custom">
+							<div class="panel-heading custom" role="tab" id="headingFour">
+								<h4 class="panel-title custom">
+									<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+										Can I create new pages?
+										<span class="fa fa-plus-circle coll-a"></span>
+									</a>
+								</h4>
+							</div>
+							<div id="collapseFour" class="panel-collapse collapse custom" aria-labelledby="headingFour" data-parent="#accordion">
+								<div class="panel-body custom">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+									book.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Clients Testimonial -->
+				<div class="r-testimonials-block-up">
+					<div class="testimonials">
+						<!-- Information -->
+						<div class="owl-carousel owl-theme">
+							<!-- Item #1 -->
+							<div class="client-block">
+								<!-- Img -->
+								<img src="img/users/1.jpg" alt="X-Data">
+								<!-- Info -->
+								<div class="info-bio">
+									<h4>User</h4>
+									<span>@ website.com</span>
+								</div>
+								<!-- FeedBack -->
+								<div class="f-back">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s .
+								</div>
+							</div>
+							<!-- Item #2 -->
+							<div class="client-block">
+								<!-- Img -->
+								<img src="img/users/2.jpg" alt="X-Data">
+								<!-- Info -->
+								<div class="info-bio">
+									<h4>User</h4>
+									<span>@ website.com</span>
+								</div>
+								<!-- FeedBack -->
+								<div class="f-back">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s .
+								</div>
+							</div>
+							<!-- Item #3 -->
+							<div class="client-block">
+								<!-- Img -->
+								<img src="img/users/3.jpg" alt="X-Data">
+								<!-- Info -->
+								<div class="info-bio">
+									<h4>User</h4>
+									<span>@ website.com</span>
+								</div>
+								<!-- FeedBack -->
+								<div class="f-back">
+									Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+									text ever since the 1500s .
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Partners -->
+			<div class="clearfix"></div>
+			<div class="partners">
+				<div class="row">
+					<!-- Item #1 -->
+					<img class="col-lg-2" src="img/partners/ad.png" alt="X-Data">
+					<!-- Item #2 -->
+					<img class="col-lg-2" src="img/partners/cc.png" alt="X-Data">
+					<!-- Item #3 -->
+					<img class="col-lg-2" src="img/partners/gr.png" alt="X-Data">
+					<!-- Item #4 -->
+					<img class="col-lg-2" src="img/partners/pd.png" alt="X-Data">
+					<!-- Item #5 -->
+					<img class="col-lg-2" src="img/partners/tf.png" alt="X-Data">
+					<!-- Item #6 -->
+					<img class="col-lg-2" src="img/partners/vh.png" alt="X-Data">
+				</div>
+			</div>
+		</div>
+		<!-- /Container -->
+
+		<!-- Footer -->
+<?php include("st_files/foot_page.php") ?>
